7f0ef10315d0ccc5e05acf25565323a7c562c248
Philip Guo <jlguo9@cse.cuhk.edu.hk>
2021-11-26 15:25:27 +0800

    opencv that we are using

---
+++ opencv/download.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt-get install build-essential
+sudo apt-get install cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
+sudo apt-get install python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev 
+sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev liblapacke-dev
+sudo apt-get install libxvidcore-dev libx264-dev
+sudo apt-get install libatlas-base-dev gfortran 
+sudo apt-get install ffmpeg
