1011a5f5566a0725740d3c214c6fb3f4e465fa50
unknown <13zgjl@sina.cn>
2021-11-29 23:40:23 +0800

    prevent interpolated value to be beyond (max,min)

--- src/interpolate.py
+++ src/interpolate.py
@@ -1,4 +1,5 @@
 from scipy.interpolate import interp2d
+from scipy.interpolate import griddata
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -10,9 +11,19 @@ cols = df.shape[1]
 arr = df.to_numpy()
 
 x = np.where(arr != 0)
+
+# points = np.column_stack((x[0],x[1]))
+# values = arr[x].flatten()
+
+# grid_x, grid_y = np.mgrid[0:5:6j, 0:4:5j]
+# grid_z = griddata(points, values, (grid_x, grid_y), method='nearest')
+# print(grid_z)
+
 f = interp2d(x[0], x[1], arr[x].flatten(), kind='linear')
 res = arr.copy()
 res[arr==0] = f(range(rows), range(cols)).T[arr==0]
+res[res>1] = 1
+res[res<0] = 0
 print(res)
 
 np.savetxt("interpolated_matrix.csv",res,delimiter=',')
