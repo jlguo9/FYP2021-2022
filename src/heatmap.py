ba1662f0db9828c5cf5bfa07d849a1eb921e1456
unknown <13zgjl@sina.cn>
2022-04-17 13:12:14 +0800

    heatmaps become smoother now

--- src/heatmap.py
+++ src/heatmap.py
@@ -1,3 +1,4 @@
+from configparser import Interpolation
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
@@ -5,18 +6,37 @@ import sys
 import numpy as np
 
 class heatmap:
+    # def showHeatMap(path, l_ref, h_ref, l_mod, h_mod):
+    #     df = pd.read_csv(path,header=None)
+    #     data = df.to_numpy()
+    #     XLabel = list(range(l_ref,h_ref+1))
+    #     YLabel = list(range(l_mod,h_mod+1))
+    #     fig = plt.figure()
+    #     ax = fig.add_subplot(111)
+    #     ax.set_xticks(range(len(XLabel)))
+    #     ax.set_xticklabels(XLabel)
+    #     ax.set_yticks(range(len(YLabel)))
+    #     ax.set_yticklabels(YLabel[::-1])
+    #     img = ax.imshow(data, cmap = plt.cm.jet)
+    #     plt.colorbar(img)
+    #     plt.title("JND Surface Visualization")
+    #     plt.savefig(path+".png")
+    #     plt.show()
+
     def showHeatMap(path, l_ref, h_ref, l_mod, h_mod):
         df = pd.read_csv(path,header=None)
         data = df.to_numpy()
         XLabel = list(range(l_ref,h_ref+1))
         YLabel = list(range(l_mod,h_mod+1))
+
         fig = plt.figure()
         ax = fig.add_subplot(111)
         ax.set_xticks(range(len(XLabel)))
         ax.set_xticklabels(XLabel)
         ax.set_yticks(range(len(YLabel)))
         ax.set_yticklabels(YLabel[::-1])
-        img = ax.imshow(data, cmap = plt.cm.jet)
+
+        img = ax.imshow(data, cmap = plt.cm.jet, interpolation="bilinear")
         plt.colorbar(img)
         plt.title("JND Surface Visualization")
         plt.savefig(path+".png")
